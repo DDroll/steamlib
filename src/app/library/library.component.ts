e557ee69220fb095fa01728e8deafc990288d49d
ddroll <d-droll@mail.ru>
2017-02-05 01:26:40 +0300

    add apps cache

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -13,13 +13,17 @@ export class LibraryComponent implements OnInit {
 
   currUser: SteamUserInfoInterface;
   hasAccount: boolean;
-  games : Array<any> = [];
+  games : Array<any>;
+  appsCache: any;
   gamesCount: number;
   gameDescr: any;
   currentApp: string;
-    slimOpts: SlimScrollOptions;
+  slimOpts: SlimScrollOptions;
 
-  constructor(private userInfo: UserInfoService, private steam: SteamService) { }
+  constructor(private userInfo: UserInfoService, private steam: SteamService) {
+      this.games = JSON.parse(localStorage.getItem('steamUserGamesList')) || [];
+      this.appsCache = JSON.parse(localStorage.getItem('appsCache')) || {};
+  }
 
   makeGameImage(appid, id){
     return `http://media.steampowered.com/steamcommunity/public/images/apps/${appid}/${id}.jpg`;
@@ -28,8 +32,11 @@ export class LibraryComponent implements OnInit {
   loadGameDescr(appid){
     if(appid !== this.currentApp){
       this.currentApp = appid;
+      this.gameDescr = this.appsCache[appid] || {};
       this.steam.getAppInfo(appid).subscribe((descr)=>{
-        this.gameDescr = JSON.parse(descr)[appid];
+          this.appsCache[appid] = JSON.parse(descr)[appid];
+          this.gameDescr = this.appsCache[appid];
+          localStorage.setItem('appsCache', JSON.stringify(this.appsCache));
       })
     }
   }
@@ -38,8 +45,7 @@ export class LibraryComponent implements OnInit {
     if(this.userInfo.checkUser()){
       this.currUser = this.userInfo.getUser();
       this.hasAccount = true;
-      if(this.games.length === 0){
-        this.steam.getUserGames(this.currUser.apikey, this.currUser.steamid).subscribe((gamesResp) => {
+      this.steam.getUserGames(this.currUser.apikey, this.currUser.steamid).subscribe((gamesResp) => {
           this.games = gamesResp.games.sort((a,b)=>{
             if(a.name > b.name){
               return 1;
@@ -49,9 +55,9 @@ export class LibraryComponent implements OnInit {
               return 0;
             }
           });
+          localStorage.setItem('steamUserGamesList', JSON.stringify(this.games));
           this.gamesCount = gamesResp.game_count;
         });
-      }
     }
       this.slimOpts = {
           position: 'right',
