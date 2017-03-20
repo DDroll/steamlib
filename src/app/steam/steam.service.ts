25bf455987977f8fff1a3831bbb4d45116bd7e71
Dmitry Lobov <d-droll@mail.ru>
2017-03-20 03:03:35 +0300

    upgrade angular cli

--- src/app/steam/steam.service.ts
+++ src/app/steam/steam.service.ts
@@ -5,8 +5,8 @@ import {Observable} from "rxjs/Observable";
 @Injectable()
 export class SteamService {
   constructor( private http:Http ) {}
-  testApiKeyAndUserId(apiKey: string, userId: string): Observable<any>{
-    return this.http.get(`/api/steam/user-info?apikey=${apiKey}&uid=${userId}`)
+  testApiKeyAndUserId(apiKey: string, nick: string): Observable<any>{
+    return this.http.get(`/api/steam/user-info?apikey=${apiKey}&nickname=${nick}`)
       .map(res => res.json())
       .map(res => res.response.players[0]);
   }
